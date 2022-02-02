91f1d1e47fcb0c295d16d51105ea1786bec61382
Ezequiel Faustino <ezequielfaustino@gmail.com>
2022-02-01 21:35:04 -0400

    mode style, script

--- slideShow.js
+++ slideShow.js
@@ -9,15 +9,29 @@ const images = [
     {'id': '5', 'url': './img/yuyuhakusho.jpg'}, 
 ];
 
-const container = document.querySelector('#container-items');
+const containerItems = document.querySelector('#container-items');
 
 const loadImage = (images, container) =>{
     images.forEach(image => {
         container.innerHTML += `
         <div class='item'>
-            <img src='${image.url}'> 
+        <img src='${image.url}'> 
         </div>`
     }) //varrer as imgs
 }
 
-loadImage(images, container);
+loadImage(images, containerItems);
+
+
+let items = document.querySelectorAll('.item');
+const previous = () =>{
+    containerItems.appendChild(items[0]);
+    items = document.querySelectorAll('.item');
+}
+
+const next = () =>{
+    
+} 
+
+document.querySelector('#prev').addEventListener('click', previous);
+document.querySelector('#next').addEventListener('click', next);
