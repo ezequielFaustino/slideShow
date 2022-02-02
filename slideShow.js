dd88ae6675a6727a3e762c1bdd6ac410b9c14316
Ezequiel Faustino <ezequielfaustino@gmail.com>
2022-02-01 20:43:24 -0400

    mode script

---
+++ slideShow.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const images = {
+    'id': '1', 'url': './img/chrono.jpg',
+    'id': '2', 'url': './img/inuyasha.jpg',
+    'id': '3', 'url': './img/tenchi.jpg',
+    'id': '4', 'url': './img/tenjhotenge.jpg',
+    'id': '5', 'url': './img/yuyuhakusho.jpg', 
+};
+
+loadImage(images);
