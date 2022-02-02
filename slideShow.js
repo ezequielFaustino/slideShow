69034c4713ab263c9a1f4970244b923b21b8e7e8
Ezequiel Faustino <ezequielfaustino@gmail.com>
2022-02-01 21:15:22 -0400

    mode style

--- slideShow.js
+++ slideShow.js
@@ -1,11 +1,23 @@
 'use strict';
 
-const images = {
-    'id': '1', 'url': './img/chrono.jpg',
-    'id': '2', 'url': './img/inuyasha.jpg',
-    'id': '3', 'url': './img/tenchi.jpg',
-    'id': '4', 'url': './img/tenjhotenge.jpg',
-    'id': '5', 'url': './img/yuyuhakusho.jpg', 
-};
+const images = [
+    
+    {'id': '1', 'url': './img/chrono.jpg'},
+    {'id': '2', 'url': './img/inuyasha.jpg'},
+    {'id': '3', 'url': './img/tenchi.jpg'},
+    {'id': '4', 'url': './img/tenjhotenge.jpg'},
+    {'id': '5', 'url': './img/yuyuhakusho.jpg'}, 
+];
 
-loadImage(images);
+const container = document.querySelector('#container-items');
+
+const loadImage = (images, container) =>{
+    images.forEach(image => {
+        container.innerHTML += `
+        <div class='item'>
+            <img src='${image.url}'> 
+        </div>`
+    }) //varrer as imgs
+}
+
+loadImage(images, container);
